85b47fe3750923f78347468ac9645cdadf777cce
sapphire-al2o3 <t_telkun@hotmail.com>
2022-01-24 23:14:11 +0900

    GameObject生成の計測

--- Assets/UnityAPITest/PrefabTest.cs
+++ Assets/UnityAPITest/PrefabTest.cs
@@ -16,5 +16,22 @@ public class PrefabTest : MonoBehaviour
             var go = GameObject.Instantiate(prefab);
             Profiler.EndSample();
         }
+
+        
+        {
+            // 40byte
+            Profiler.BeginSample("new GameObject");
+            var go = new GameObject();
+            Profiler.EndSample();
+
+            // 0byte
+            Profiler.BeginSample("set_name");
+            go.name = "test";
+            Profiler.EndSample();
+
+            Profiler.BeginSample("SetParent");
+            go.transform.SetParent(transform);
+            Profiler.EndSample();
+        }
     }
 }
