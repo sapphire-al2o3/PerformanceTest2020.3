8cb82db7d21b8aa5e1f04b760d93193fe53ba14f
sapphire-al2o3 <t_telkun@hotmail.com>
2022-01-26 00:25:20 +0900

    new GameObjectとInstantiateの比較

--- Assets/UnityAPITest/PrefabTest.cs
+++ Assets/UnityAPITest/PrefabTest.cs
@@ -8,6 +8,9 @@ public class PrefabTest : MonoBehaviour
     [SerializeField]
     GameObject prefab;
 
+    [SerializeField]
+    GameObject empty;
+
     void Start()
     {
         // 152byte
@@ -15,9 +18,35 @@ public class PrefabTest : MonoBehaviour
             Profiler.BeginSample("prefab Instantiate");
             var go = GameObject.Instantiate(prefab);
             Profiler.EndSample();
+
+            Profiler.BeginSample("SetParent");
+            go.transform.SetParent(transform);
+            Profiler.EndSample();
         }
 
-        
+        {
+            Profiler.BeginSample("prefab(empty) Instantiate");
+            var go = GameObject.Instantiate(empty);
+            Profiler.EndSample();
+        }
+
+        {
+            Profiler.BeginSample("prefab(empty) Instantiate x100");
+            for (int i = 0; i < 100; i++)
+            {
+                var tmp = GameObject.Instantiate(empty, transform);
+                var x = tmp.transform.position.x;
+            }
+            Profiler.EndSample();
+        }
+
+        {
+            Profiler.BeginSample("prefab Instantiate & SetParnet");
+            var go = GameObject.Instantiate(prefab, transform);
+            Profiler.EndSample();
+        }
+
+
         {
             // 40byte
             Profiler.BeginSample("new GameObject");
@@ -28,9 +57,15 @@ public class PrefabTest : MonoBehaviour
             Profiler.BeginSample("set_name");
             go.name = "test";
             Profiler.EndSample();
+        }
 
-            Profiler.BeginSample("SetParent");
-            go.transform.SetParent(transform);
+        {
+            Profiler.BeginSample("new GameObject x100");
+            for (int i = 0; i < 100; i++)
+            {
+                var go = new GameObject();
+                go.transform.SetParent(transform);
+            }
             Profiler.EndSample();
         }
     }
