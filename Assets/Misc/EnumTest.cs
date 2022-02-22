9aa23f774b557622422a6cab6eaefd5b2f7143bf
sapphire-al2o3 <t_telkun@hotmail.com>
2022-02-22 20:23:09 +0900

    Enum計測用のシーン追加

--- Assets/Misc/EnumTest.cs
+++ Assets/Misc/EnumTest.cs
@@ -5,23 +5,39 @@ using UnityEngine.Profiling;
 
 public class EnumTest : MonoBehaviour
 {
-	[System.Flags]
-	enum Flags
-	{
-		A,
-		B,
-		C
-	}
+    [System.Flags]
+    enum Flags
+    {
+        A,
+        B,
+        C
+    }
 
     void Start()
     {
-		{
-			Profiler.BeginSample("HasFlags");
-			Flags flag = Flags.A;
-			bool ret = flag.HasFlag(Flags.B);
-			Profiler.EndSample();
+        // IL‚ð‚Ý‚é‚Æbox‚ª‚æ‚Ñ‚¾‚³‚ê‚Ä‚¢‚é‚¯‚Ç0byte
+        // 0byte
+        {
+            Profiler.BeginSample("Enum.HasFlags");
+            Flags flag = Flags.A;
+            bool ret = flag.HasFlag(Flags.B);
+            for (int i = 0; i < 100; i++)
+            {
+                ret |= flag.HasFlag(Flags.C);
+            }
+            Profiler.EndSample();
 
-			Debug.Log(ret);
-		}
+            Debug.Log(ret);
+        }
+
+        // 20byte
+        {
+            Profiler.BeginSample("Enum.ToObject");
+            int i = 0;
+            Flags flag = (Flags)System.Enum.ToObject(typeof(Flags), i);
+            Profiler.EndSample();
+
+            Debug.Log(flag);
+        }
     }
 }
