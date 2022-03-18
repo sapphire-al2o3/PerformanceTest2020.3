eadf15e1b2ad0d733e1101a8b7679ca33436bac1
sapphire-al2o3 <t_telkun@hotmail.com>
2022-03-18 18:35:08 +0900

    メーター表示でメッシュを自動生成する

--- Assets/FPSMeter/FPSMeter.cs
+++ Assets/FPSMeter/FPSMeter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class FPSMeter : MonoBehaviour
 {
@@ -26,9 +27,46 @@ public class FPSMeter : MonoBehaviour
 
     int sizeID;
     int colorID;
+    Mesh mesh;
+    
+
+    void OnDestroy()
+    {
+        if (mesh != null)
+        {
+            Destroy(mesh);
+        }
+    }
 
     void Start()
     {
+        mesh = new Mesh();
+        int[] indices = new int[6];
+        Vector2[] uvs = new Vector2[4];
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(0, 0, 0);
+        vertices[2] = new Vector3(0, 0, 0);
+        vertices[3] = new Vector3(0, 0, 0);
+
+        uvs[0] = new Vector2(0, 0);
+        uvs[1] = new Vector2(1, 0);
+        uvs[2] = new Vector2(0, 1);
+        uvs[3] = new Vector2(1, 1);
+
+        indices[0] = 0;
+        indices[1] = 1;
+        indices[2] = 2;
+        indices[3] = 2;
+        indices[4] = 1;
+        indices[5] = 3;
+
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.uv = uvs;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
         sizeID = Shader.PropertyToID("_Size");
         mat = GetComponent<Renderer>().material;
         size = mat.GetVector(sizeID);
@@ -36,6 +74,14 @@ public class FPSMeter : MonoBehaviour
         mat.SetVector(sizeID, size);
         colorID = Shader.PropertyToID("_Color");
         defaultColor = mat.GetColor(colorID);
+
+        //var sampler = UnityEngine.Profiling.CustomSampler.Create("hoge", true);
+        //var commandBuffer = new CommandBuffer();
+        //commandBuffer.BeginSample(sampler);
+        //commandBuffer.EndSample(sampler);
+        //Camera.main.AddCommandBuffer(CameraEvent.AfterImageEffects, commandBuffer);
+
+        
     }
 
     void Update()
