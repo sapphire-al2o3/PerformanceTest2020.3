38e9a13e169922da84313cc5a9e3cd27ec1ff902
sapphire-al2o3 <t_telkun@hotmail.com>
2023-01-19 18:55:14 +0900

    改行コード修正

--- Assets/Misc/TimeTest.cs
+++ Assets/Misc/TimeTest.cs
@@ -5,24 +5,24 @@ using UnityEngine.Profiling;
 
 public class TimeTest : MonoBehaviour
 {
-	void Start()
-	{
-		{
-			Profiler.BeginSample("Now");
-			for (int i = 0; i < 100; i++)
-			{
-				var time = System.DateTime.Now;
-			}
-			Profiler.EndSample();
-		}
+    void Start()
+    {
+        {
+            Profiler.BeginSample("Now");
+            for (int i = 0; i < 100; i++)
+            {
+                var time = System.DateTime.Now;
+            }
+            Profiler.EndSample();
+        }
 
-		{
-			Profiler.BeginSample("UtcNow");
-			for (int i = 0; i < 100; i++)
-			{
-				var time = System.DateTime.UtcNow;
-			}
-			Profiler.EndSample();
-		}
-	}
+        {
+            Profiler.BeginSample("UtcNow");
+            for (int i = 0; i < 100; i++)
+            {
+                var time = System.DateTime.UtcNow;
+            }
+            Profiler.EndSample();
+        }
+    }
 }
